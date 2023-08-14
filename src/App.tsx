7c6eaee2e726ffe911909bb7071e1906cce23fde
Neat <sophaneat@student.fdu.edu>
2023-08-14 15:01:03 -0700

    Change mixin

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 // Component
 import Header from './components/global/Header/Header';
 // Page
 import HomePage from './pages/HomePage/HomePage';
 import Register from './pages/Register/Register';
 import RegisterFormPage from './pages/RegisterFormPage/RegisterFormPage';
-
-import './App.css';
 
 function App() {
 
@@ -17,8 +15,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/register" element={<Register />} /> */}
-        {/* <Route path="/register/:type" element={<RegisterFormPage />} /> */}
+        <Route path="/register" element={<Register />} /> 
+        <Route path="/register/:type" element={<RegisterFormPage />} />
       </Routes>
     </Router>
   );
