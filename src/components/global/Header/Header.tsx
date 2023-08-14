5f1993eaca5f7802e20fa0981f6af691b508c795
stuckinsnow <126236947+stuckinsnow@users.noreply.github.com>
2023-08-14 13:04:00 -0700

    style links.

--- src/components/global/Header/Header.tsx
+++ src/components/global/Header/Header.tsx
@@ -1,11 +1,9 @@
 import './Header.scss';
-import { useEffect, useState, useRef } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { useToggleClass } from '../../../utils/functions';
 
-
 const Header = () => {
-    const [isHamburgerOpen, toggleHamburger, setIsHamburgerClicked] = useToggleClass(false);
+    const [isHamburgerOpen, toggleHamburger] = useToggleClass(false);
 
     const handleNavLinkClick = () => {
         if (isHamburgerOpen) {
@@ -21,27 +19,30 @@ const Header = () => {
 
             {/* Hamburger menu */}
             <div className={`hamburger ${isHamburgerOpen ? 'active' : ''}`} onClick={toggleHamburger}>
-                <span className="hamburger__bar hamburger__bar-1"></span>
-                <span className="hamburger__bar hamburger__bar-2"></span>
-                <span className="hamburger__bar hamburger__bar-3"></span>
+                <span className="hamburger__bar hamburger__bar--1"></span>
+                <span className="hamburger__bar hamburger__bar--2"></span>
+                <span className="hamburger__bar hamburger__bar--3"></span>
             </div>
 
             {/* Navbar */}
             <nav className={`navbar ${isHamburgerOpen ? 'active' : ''}`}>
                 <ul className='navbar__list'>
                     <li className={`navbar__item navbar__item-1 navbar__item-1`}>
-                        <Link to="/contact" className="navbar__link navbar__link--request btn" onClick={handleNavLinkClick}>
-                            Lorem, ipsum dolor.
-                        </Link>
+                        <NavLink to="/contact" className="navbar__link" onClick={handleNavLinkClick}>
+                            <span className="navbar__link--first hidden-large">01</span>
+                            <span className="navbar__link--second">Home</span>
+                        </NavLink>
                     </li>
                     <li className="navbar__item">
-                        <NavLink to="/projects/" className="navbar__link link-animate" onClick={handleNavLinkClick} >
-                            Lorem, ipsum.
+                        <NavLink to="/projects/" className="navbar__link" onClick={handleNavLinkClick} >
+                            <span className="navbar__link--first hidden-large">02</span>
+                            <span className="navbar__link--second">Explore</span>
                         </NavLink>
                     </li>
                     <li className='navbar__item'>
-                        <NavLink to="/about" className="navbar__link link-animate" onClick={handleNavLinkClick} >
-                            Lorem.
+                        <NavLink to="/about" className="navbar__link" onClick={handleNavLinkClick} >
+                            <span className="navbar__link--first hidden-large">03</span>
+                            <span className="navbar__link--second">About</span>
                         </NavLink>
                     </li>
                 </ul>
