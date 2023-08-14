0f4933f80c052f24ebd1d8301b1445c050a4850e
stuckinsnow <126236947+stuckinsnow@users.noreply.github.com>
2023-08-14 14:55:27 -0700

    fix z-index error.

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,7 +4,11 @@ const HomePage = () => {
     useDocumentTitle("Home Page")
 
     return (
-        <></>
+        <>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, delectus quidem? Sunt eius qui adipisci animi est saepe neque totam iure! Soluta culpa rem, blanditiis sequi provident ullam! Reiciendis, sit, laboriosam, dolore quas assumenda saepe commodi quia voluptatem iste error fuga neque blanditiis autem quae vel. Modi mollitia, illum, vitae officiis minima fugit ullam dignissimos nihil alias, repellat nisi dolorem ut laboriosam aliquid eligendi deleniti nulla consectetur magnam vel magni. Magnam sapiente veniam repudiandae repellendus eaque aperiam rerum doloribus excepturi quaerat error quasi quam aspernatur sint dolor nisi, similique nostrum. Magni, exercitationem corporis ducimus blanditiis non atque quisquam odit ad?
+
+
+        </>
     )
 }
 
