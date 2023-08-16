2dcab26afcc3f063322c1cdd23e4e112d6a5d861
Philip <126236947+stuckinsnow@users.noreply.github.com>
2023-08-15 20:16:54 -0700

    change global function.

--- src/components/global/FilterButton/FilterButton.tsx
+++ src/components/global/FilterButton/FilterButton.tsx
@@ -1,11 +1,21 @@
 import './FilterButton.scss';
-
+import { useToggleClass } from '../../../utils/functions';
 
 const FilterButton = () => {
+
+    const [isSwitchToggled, toggleSwitch] = useToggleClass(false);
+
+
+
+
     return (
         <>
 
-            <div className="filter-btn">
+            <div
+
+                className={`filter-btn ${isSwitchToggled ? 'active' : ''}`}
+
+                onClick={toggleSwitch}>
                 <span className="filter-btn__event">Event</span>
                 <span className="filter-btn__business">Business</span>
             </div>
