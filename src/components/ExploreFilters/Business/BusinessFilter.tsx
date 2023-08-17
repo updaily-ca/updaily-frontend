adb11b62b61d86964d26aa7d9e031cf85d1cedec
Philip <126236947+stuckinsnow@users.noreply.github.com>
2023-08-16 19:16:07 -0700

    make css pretty.

--- src/components/ExploreFilters/Business/BusinessFilter.tsx
+++ src/components/ExploreFilters/Business/BusinessFilter.tsx
@@ -14,58 +14,64 @@ const BusinessFilter = ({ searchTerm, setSearchTerm, handleKeyDown, handleSearch
 
             {/* <div className="c-businessfilter__search"> */}
 
-            <label className='c-businessfilter__search-title'>Search by location</label>
-            <input
-                type="text"
-                className="c-businessfilter__search-input"
-                placeholder=""
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                onKeyDown={handleKeyDown}
-            />
+            <div className="filter-card">
 
-            <div className="group-filter">
-
-                <label className="group-filter__label">Price Range</label>
-                <input type="text" className="group-filter__input group-filter__input--price-min" />
-                <input type="text" className="group-filter__input group-filter__input--price-max" />
+                <label className='filter-card__subtitle'>Search by location</label>
+                <input
+                    type="text"
+                    className="filter-card__input filter-card__input--search"
+                    placeholder=""
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                />
 
             </div>
 
-            <div className="group-filter">
 
-                <div className="group-filter__label">Opening Times</div>
 
-                <div className="group-filter__btn">Morning</div>
-                <div className="group-filter__btn">Afternoon</div>
-                <div className="group-filter__btn">Evening</div>
-                <div className="group-filter__btn">Open Now</div>
+            <div className="filter-card">
 
-            </div>
-
-            <div className="group-filter">
-
-                <div className="group-filter__label">Search</div>
-
-                <div className="group-filter__btn">Food and drink</div>
-                <div className="group-filter__btn">Nature</div>
-                <div className="group-filter__btn">Art and culture</div>
-                <div className="group-filter__btn">Technology</div>
-
-                <div className="group-filter__btn">Sports</div>
-                <div className="group-filter__btn">Hiking & Camping</div>
-                <div className="group-filter__btn">Entertainment</div>
-                <div className="group-filter__btn">Other</div>
+                <label className="filter-card__subtitle">Price Range</label>
+                <input type="text" className="filter-card__input filter-card__input--price-min" />
+                <input type="text" className="filter-card__input filter-card__input--price-max" />
 
             </div>
 
-            <div className="group-filter">
+            <div className="filter-card">
 
-                <div className="group-filter__label">Business Age</div>
+                <div className="filter-card__subtitle">Opening Times</div>
 
-                <div className="group-filter__btn">Newest</div>
-                <div className="group-filter__btn">This Year</div>
-                <div className="group-filter__btn">Any</div>
+                <div className="filter-card__btn">Morning</div>
+                <div className="filter-card__btn">Afternoon</div>
+                <div className="filter-card__btn">Evening</div>
+                <div className="filter-card__btn">Open Now</div>
+
+            </div>
+
+            <div className="filter-card">
+
+                <div className="filter-card__subtitle">Search</div>
+
+                <div className="filter-card__btn">Food and drink</div>
+                <div className="filter-card__btn">Nature</div>
+                <div className="filter-card__btn">Art and culture</div>
+                <div className="filter-card__btn">Technology</div>
+
+                <div className="filter-card__btn">Sports</div>
+                <div className="filter-card__btn">Hiking & Camping</div>
+                <div className="filter-card__btn">Entertainment</div>
+                <div className="filter-card__btn">Other</div>
+
+            </div>
+
+            <div className="filter-card">
+
+                <div className="filter-card__subtitle">Business Age</div>
+
+                <div className="filter-card__btn">Newest</div>
+                <div className="filter-card__btn">This Year</div>
+                <div className="filter-card__btn">Any</div>
 
             </div>
 
