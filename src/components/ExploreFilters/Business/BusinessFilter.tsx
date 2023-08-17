4fbc0a2ac46f3daf4907108ef497314d272a6db7
Philip <126236947+stuckinsnow@users.noreply.github.com>
2023-08-16 19:42:35 -0700

    add search categories.

--- src/components/ExploreFilters/Business/BusinessFilter.tsx
+++ src/components/ExploreFilters/Business/BusinessFilter.tsx
@@ -1,88 +1,64 @@
-import './BusinessFilter.scss';
+import { businessType } from "../../../utils/FormData"
+import "./BusinessFilter.scss"
 
 interface BusinessFilterProps {
-    searchTerm: string,
-    setSearchTerm: (newValues: string) => void,
-    handleKeyDown: (event: React.KeyboardEvent<HTMLInputElement>) => void,
+    searchTerm: string
+    setSearchTerm: (newValues: string) => void
+    handleKeyDown: (event: React.KeyboardEvent<HTMLInputElement>) => void
     handleSearchClick: () => void
 }
-const BusinessFilter = ({ searchTerm, setSearchTerm, handleKeyDown, handleSearchClick, }: BusinessFilterProps) => {
-
+const BusinessFilter = ({ searchTerm, setSearchTerm, handleKeyDown, handleSearchClick }: BusinessFilterProps) => {
     return (
         <div className="c-businessfilter">
-            {/* This should only show if the business filter has been toggled. */}
-
-            {/* <div className="c-businessfilter__search"> */}
 
             <div className="filter-card">
-
-                <label className='filter-card__subtitle'>Search by location</label>
-                <input
-                    type="text"
-                    className="filter-card__input filter-card__input--search"
-                    placeholder=""
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    onKeyDown={handleKeyDown}
-                />
-
+                <label className="filter-card__subtitle">Search by location</label>
+                <input type="text" className="filter-card__input filter-card__input--search" placeholder="" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} onKeyDown={handleKeyDown} />
             </div>
 
-
-
             <div className="filter-card">
-
                 <label className="filter-card__subtitle">Price Range</label>
                 <input type="text" className="filter-card__input filter-card__input--price-min" />
                 <input type="text" className="filter-card__input filter-card__input--price-max" />
-
             </div>
 
             <div className="filter-card">
-
                 <div className="filter-card__subtitle">Opening Times</div>
 
                 <div className="filter-card__btn">Morning</div>
                 <div className="filter-card__btn">Afternoon</div>
                 <div className="filter-card__btn">Evening</div>
                 <div className="filter-card__btn">Open Now</div>
-
             </div>
 
             <div className="filter-card">
-
                 <div className="filter-card__subtitle">Search</div>
 
-                <div className="filter-card__btn">Food and drink</div>
-                <div className="filter-card__btn">Nature</div>
-                <div className="filter-card__btn">Art and culture</div>
-                <div className="filter-card__btn">Technology</div>
-
-                <div className="filter-card__btn">Sports</div>
-                <div className="filter-card__btn">Hiking & Camping</div>
-                <div className="filter-card__btn">Entertainment</div>
-                <div className="filter-card__btn">Other</div>
+                {businessType
+                    // .sort((a, b) => a.localeCompare(b))
+                    .map((businessType) => (
+                        <div key={businessType} className="uc filter-card__btn">
+                            {businessType}
+                        </div>
+                    ))}
 
             </div>
 
             <div className="filter-card">
-
                 <div className="filter-card__subtitle">Business Age</div>
 
                 <div className="filter-card__btn">Newest</div>
                 <div className="filter-card__btn">This Year</div>
                 <div className="filter-card__btn">Any</div>
-
             </div>
 
-
-            <div className="c-businessfilter__search-btn" onClick={handleSearchClick}>Search
+            <div className="c-businessfilter__search-btn" onClick={handleSearchClick}>
+                Search
             </div>
-
 
             {/* </div> */}
         </div>
-    );
-};
+    )
+}
 
-export default BusinessFilter;
+export default BusinessFilter
--- src/pages/ExplorePage/ExplorePage.tsx
+++ src/pages/ExplorePage/ExplorePage.tsx
@@ -1,74 +1,66 @@
-import BusinessFilter from '../../components/ExploreFilters/Business/BusinessFilter';
-import EventFilter from '../../components/ExploreFilters/Event/EventFilter';
-import FilterButton from '../../components/global/FilterButton/FilterButton';
-import SearchCards from '../../components/global/SearchCards/SearchCards';
-import { performSearch } from '../../utils/functions';
-import { useEffect, useState } from 'react';
+import BusinessFilter from "../../components/ExploreFilters/Business/BusinessFilter"
+import EventFilter from "../../components/ExploreFilters/Event/EventFilter"
+import FilterButton from "../../components/global/FilterButton/FilterButton"
+import SearchCards from "../../components/global/SearchCards/SearchCards"
+import { performSearch } from "../../utils/functions"
+import { useEffect, useState } from "react"
 
-import './ExplorePage.scss';
-import { useDocumentTitle } from '../../utils/functions';
+import "./ExplorePage.scss"
+import { useDocumentTitle } from "../../utils/functions"
 
 const ExplorePage = () => {
-    useDocumentTitle('Explore Page');
+    useDocumentTitle("Explore Page")
 
-    const [isFilterBusiness, setIsFilterBusiness] = useState<boolean>(false);
+    const [isFilterBusiness, setIsFilterBusiness] = useState<boolean>(false)
 
     const toggleBusinessMode = (): void => {
-        setIsFilterBusiness(!isFilterBusiness);
-    };
+        setIsFilterBusiness(!isFilterBusiness)
+    }
 
-    const [searchTerm, setSearchTerm] = useState<string>('');
-    const [prevSearchTerm, setPrevSearchTerm] = useState<string>('');
+    const [searchTerm, setSearchTerm] = useState<string>("")
+    const [prevSearchTerm, setPrevSearchTerm] = useState<string>("")
 
     const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-        if (event.key === 'Enter') {
-            performSearch(searchTerm, prevSearchTerm, setPrevSearchTerm);
+        if (event.key === "Enter") {
+            performSearch(searchTerm, prevSearchTerm, setPrevSearchTerm)
         }
-    };
+    }
 
     const handleSearchClick = (): void => {
-        performSearch(searchTerm, prevSearchTerm, setPrevSearchTerm);
-    };
+        performSearch(searchTerm, prevSearchTerm, setPrevSearchTerm)
+    }
 
     // useEffect(() => {
     //     console.log(searchTerm, isFilterBusiness);
     // }, [searchTerm, isFilterBusiness]);
 
     return (
-        <div id="p-explorepage"> {/* page - explore page */}
-
+        <div id="p-explorepage">
+            {" "}
+            {/* page - explore page */}
             <aside className="filter-container">
                 <div className="filters">
-
                     <div className="filters__header">
-
-                        <div className="filters__title"> Filters
-                        </div>
+                        <div className="filters__title"> Filters</div>
                         <FilterButton isBusinessMode={isFilterBusiness} toggleBusinessMode={toggleBusinessMode} />
                     </div>
 
                     {/*  This needs optimising because there's a function we can use in the utils function file */}
 
                     {isFilterBusiness ? <BusinessFilter searchTerm={searchTerm} setSearchTerm={setSearchTerm} handleKeyDown={handleKeyDown} handleSearchClick={handleSearchClick} /> : <EventFilter />}
-
                 </div>
-
             </aside>
-
             <div className="map-container">
+                <div className="map">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente explicabo optio illo cupiditate laborum? Deserunt et eaque blanditiis culpa nulla?</div>
 
-                <div className="map">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente explicabo optio illo cupiditate laborum? Deserunt et eaque blanditiis culpa nulla?
-                </div>
-
-                <div className="e-cc-searchcards"> {/* explore page - component container - search cards */}
+                <div className="e-cc-searchcards">
+                    {" "}
+                    {/* explore page - component container - search cards */}
                     <SearchCards />
                 </div>
-
             </div>
-
         </div>
     )
 }
 
-export default ExplorePage;
+export default ExplorePage
