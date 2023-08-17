ff658feb4ae2aa482bb9cdc3bd32be9963a83c6c
stuckinsnow <126236947+stuckinsnow@users.noreply.github.com>
2023-08-17 11:35:48 -0700

    add detail page.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import HomePage from './pages/HomePage/HomePage';
 import Register from './pages/Register/Register';
 import RegisterFormPage from './pages/RegisterFormPage/RegisterFormPage';
 import ExplorePage from './pages/ExplorePage/ExplorePage';
+import DetailPage from './pages/DetailPage/DetailPage';
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/register/:type" element={<RegisterFormPage />} />
         <Route path="/explore" element={<ExplorePage />} />
+        <Route path="/detail" element={<DetailPage />} />
       </Routes>
     </Router>
   );
---
+++ src/components/DetailPhotos/DetailPhotos.tsx
@@ -0,0 +1,9 @@
+import './DetailPhotos.scss';
+
+const DetailPhotos = () => {
+    return (
+        <div className="c-detailphotos">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat expedita voluptas dolorem provident a ducimus aliquam iure ex exercitationem optio eaque odit, laboriosam tempore rerum perferendis placeat temporibus voluptatibus voluptate!</div>
+    )
+}
+
+export default DetailPhotos;
