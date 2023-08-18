c04b3286d2e4e9f962d70783cc1b9195ba0c6b55
stuckinsnow <126236947+stuckinsnow@users.noreply.github.com>
2023-08-17 19:36:46 -0700

    pretty the code.

--- src/pages/ExplorePage/ExplorePage.tsx
+++ src/pages/ExplorePage/ExplorePage.tsx
@@ -14,7 +14,7 @@ const ExplorePage = () => {
     const [isFilterBusiness, setIsFilterBusiness] = useState<boolean>(false)
 
     const toggleBusinessMode = (): void => {
-        setIsFilterBusiness(prevState => !prevState);
+        setIsFilterBusiness((prevState) => !prevState)
     }
 
     const [searchTerm, setSearchTerm] = useState<string>("")
@@ -30,37 +30,30 @@ const ExplorePage = () => {
         performSearch(searchTerm, prevSearchTerm, setPrevSearchTerm)
     }
 
-    const [isFilterButtonClicked, toggleFilterButton] = useToggleClass(false);
-
-    // useEffect(() => {
-    //     console.log(searchTerm, isFilterBusiness);
-    // }, [searchTerm, isFilterBusiness]);
+    const [isFilterButtonClicked, toggleFilterButton] = useToggleClass(false)
 
     return (
         <div id="p-explorepage">
-            {" "}
             {/* page - explore page */}
             <aside className="filter-container">
                 <div className="filters">
                     <div className="filters__header">
-                        <div onClick={toggleFilterButton}
-
-                            className="filters__title"> Filters</div>
+                        <div onClick={toggleFilterButton} className="filters__title">
+                            {" "}
+                            Filters
+                        </div>
                         <FilterButton isBusinessMode={isFilterBusiness} toggleBusinessMode={toggleBusinessMode} />
                     </div>
 
                     {/*  This needs optimising because there's a function we can use in the utils function file */}
 
-                    {isFilterBusiness ? <BusinessFilter isFilterButtonClicked={isFilterButtonClicked}
-
-                        searchTerm={searchTerm} setSearchTerm={setSearchTerm} handleKeyDown={handleKeyDown} handleSearchClick={handleSearchClick} /> : <EventFilter />}
+                    {isFilterBusiness ? <BusinessFilter isFilterButtonClicked={isFilterButtonClicked} searchTerm={searchTerm} setSearchTerm={setSearchTerm} handleKeyDown={handleKeyDown} handleSearchClick={handleSearchClick} /> : <EventFilter />}
                 </div>
             </aside>
             <div className="map-container">
                 <div className="map">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente explicabo optio illo cupiditate laborum? Deserunt et eaque blanditiis culpa nulla?</div>
 
                 <div className="e-cc-searchcards">
-                    {" "}
                     {/* explore page - component container - search cards */}
                     <SearchCards isBusinessMode={isFilterBusiness} />
                 </div>
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,71 +1,57 @@
-import { performSearch, useDocumentTitle } from "../../utils/functions";
-import { useState } from "react";
+import { performSearch, useDocumentTitle } from "../../utils/functions"
+import { useState } from "react"
 
-import './HomePage.scss';
+import SearchCards from "../../components/global/SearchCards/SearchCards"
+import FilterButton from "../../components/global/FilterButton/FilterButton"
 
-import searchIcon from '../../asset/home/search-icon.png';
-import SearchCards from "../../components/global/SearchCards/SearchCards";
-import FilterButton from "../../components/global/FilterButton/FilterButton";
+import "./HomePage.scss"
+
+import searchIcon from "../../asset/home/search-icon.png"
 
 const HomePage = () => {
-    useDocumentTitle("Home Page");
+    useDocumentTitle("Home Page")
 
-    const [isFilterBusiness, setIsFilterBusiness] = useState<boolean>(false);
+    const [isFilterBusiness, setIsFilterBusiness] = useState<boolean>(false)
     const toggleBusinessMode = (): void => {
-        setIsFilterBusiness(prevState => !prevState);
-    };
+        setIsFilterBusiness((prevState) => !prevState)
+    }
 
-    const [searchTerm, setSearchTerm] = useState<string>('');
-    const [prevSearchTerm, setPrevSearchTerm] = useState<string>('');
+    const [searchTerm, setSearchTerm] = useState<string>("")
+    const [prevSearchTerm, setPrevSearchTerm] = useState<string>("")
 
     const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-        if (event.key === 'Enter') {
-            performSearch(searchTerm, prevSearchTerm, setPrevSearchTerm);
+        if (event.key === "Enter") {
+            performSearch(searchTerm, prevSearchTerm, setPrevSearchTerm)
         }
-    };
+    }
 
     const handleSearchClick = () => {
-        performSearch(searchTerm, prevSearchTerm, setPrevSearchTerm);
-    };
-
-    // useEffect(() => {
-    //     console.log(searchTerm, isFilterBusiness);
-    // }, [searchTerm, isFilterBusiness]);
+        performSearch(searchTerm, prevSearchTerm, setPrevSearchTerm)
+    }
 
     return (
-        <div id="p-home-page"> {/* page - home page */}
-
+        <div id="p-home-page">
+            {/* page - home page */}
             <section className="home-hero">
                 <h1 className="home-hero__title">We make exploring Vancouver quick and easy!</h1>
                 <p className="home-hero__description">Search for restaurants, events or businesses in Vancouver.</p>
 
                 <div className="home-search">
-                    <input
-                        type="text"
-                        className="home-search__input"
-                        placeholder=""
-                        value={searchTerm}
-                        onChange={(e) => setSearchTerm(e.target.value)}
-                        onKeyDown={handleKeyDown}
-                    />
+                    <input type="text" className="home-search__input" placeholder="" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} onKeyDown={handleKeyDown} />
                     <div className="home-search__btn" onClick={handleSearchClick}>
                         <img className="home-search__btn--icon" src={searchIcon} alt="" />
-                        <span className="home-search__btn--search">Search</span></div>
-
+                        <span className="home-search__btn--search">Search</span>
+                    </div>
                 </div>
             </section>
-
-            <section className="h-cc-searchcards"> {/* home page - component container - search cards */}
+            <section className="h-cc-searchcards">
+                {/* home page - component container - search cards */}
                 <h1 className="h-cc-searchcards__title">New events to explore this week</h1>
                 <SearchCards isBusinessMode={isFilterBusiness} />
             </section>
-
             <FilterButton isBusinessMode={isFilterBusiness} toggleBusinessMode={toggleBusinessMode} />
-
         </div>
     )
 }
 
-export default HomePage;
-
-
+export default HomePage
