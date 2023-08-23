f8588c888ae5faf0ed7f9576462082082328a51f
Neat <sophaneat@student.fdu.edu>
2023-08-23 11:17:48 -0700

    Add addBusiness graphql query

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -8,3 +8,47 @@ export const getFeaturedBusiness = gql `
         }
     }
 `
+
+export const addBusiness = gql`
+    mutation AddBusiness(
+        $name: String,
+        $email: String,
+        $phone: String,
+        $lat: Float,
+        $lng: Float,
+        $address: String,
+        $launch: Int,
+        $website: String,
+        $photos: [String],
+        $type: String,
+        $subtype: String,
+        $cuisine: String,
+        $openinghours: [String],
+        $pricerange: String,
+        $description: String,
+        $menu: [String],
+        $user_id: Int
+    ){
+        addBusiness(
+            name: $name,
+            email: $email,
+            phone: $phone,
+            lat: $lat,
+            lng: lng,
+            address: $address,
+            launch: $launch,
+            website: $website,
+            photos: $photo,
+            type: $type,
+            subtype: $subtype,
+            cuisine: cuisine,
+            openinghours: $openinghours,
+            pricerange: $pricerange,
+            description: $description,
+            menu: $menu,
+            user_id: $user_id
+        ) {
+            id
+        }
+    }
+`
