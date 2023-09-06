5a3746fcabb4e734084f2319f2da18368a2b7fed
Neat <sophaneat@student.fdu.edu>
2023-09-06 13:41:59 -0700

    Switch to text area

--- src/components/Forms/Input/Input.tsx
+++ src/components/Forms/Input/Input.tsx
@@ -5,13 +5,21 @@ interface InputProps {
     name: string,
     value: string | number,
     handleChange: (newValue: string) => void,
-    type?: string
+    type?: string,
+    textarea?: string
 }
-const Input = ({id, name, value, handleChange, type}: InputProps) => {
+const Input = ({id, name, value, handleChange, type, textarea}: InputProps) => {
     return (
         <div className="input">
             <label htmlFor={id} className="input__label">{name}</label>
-            <input type={type ? type : "text"} className="input__input" id={id} value={value} onChange={(e)=>handleChange(e.target.value)}/>
+            {
+                textarea ? (
+                    <textarea rows={5} className="input__input input__text-area" id={id} value={value} onChange={(e)=>handleChange(e.target.value)} />
+                ) : (
+                    <input type={type ? type : "text"} className="input__input" id={id} value={value} onChange={(e)=>handleChange(e.target.value)}/>
+                )
+            }
+            
         </div>
     )
 }
