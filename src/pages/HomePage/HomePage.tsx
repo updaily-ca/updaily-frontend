75dd0cddc33203d37244b965245dd792f7320555
Philip <126236947+stuckinsnow@users.noreply.github.com>
2023-11-04 14:15:56 -0700

    Update HomePage.tsx
    Change businesses

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -36,7 +36,7 @@ const HomePage = () => {
 
     const { data } = useQuery(getFeaturedBusiness);
 
-    const businesses = data?.businesses?.slice(0, 5);
+    const businesses = data?.businesses?.slice(0, 4);
 
     const [searchTerm, setSearchTerm] = useState<string>("");
 
